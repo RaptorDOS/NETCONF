a943b6f568c7b67433c518fc565e3e476c0160b5
RaptorDOS <43664460+RaptorDOS@users.noreply.github.com>
2020-03-06 14:12:36 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,3 @@
+sudo apt-get update && sudo apt-get -y upgrade && sudo apt-get -y dist-upgrade && sudo apt-get -y autoremove
+sudo reboot now
+sudo apt-get -y install python python-pip nginx redis-server supervisor libssl-dev libsasl2-dev gunicorn git
